b7d71262d9ad604485846264c215129766d2bc77
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-09-06 18:20:09 +0300

    Passed al tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,46 @@
 class Pet:
-    pass
+    PET_TYPES = [
+        "dog", 
+        "cat", 
+        "rodent", 
+        "bird", 
+        "reptile", 
+        "exotic"
+    ]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner = None) -> None:
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, value):
+        if value not in Pet.PET_TYPES:
+            raise ValueError('Pet type not found on your system, the pet must include: {}'.format(",".join(Pet.PET_TYPES)))
+        else:
+            self._pet_type = value
+    
 
 class Owner:
-    pass
+    def __init__(self, name) -> None:
+        self.name = name
+
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self ]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise ValueError("")
+
+    def get_sorted_pets(self):
+         return sorted([pet for pet in Pet.all if pet.owner == self], key=lambda pet: pet.name)
